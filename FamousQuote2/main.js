dda955bbb832de935454e97e45bb3654d3c9ad63
humayune01 <162161731+humayune01@users.noreply.github.com>
2024-03-05 17:56:20 +0500

    Exercise 5 - Famous Quote 2
    Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.

---
+++ FamousQuote2/main.js
@@ -0,0 +1,3 @@
+var famous_person = "Hazrat Ali bin Abi Talib (R.A)";
+var message = '[famous_person] once said, "A moment of patience in the moment of anger prevents thousands of moments of regret."';
+console.log(message.replace('[famous_person]', famous_person));
---
+++ FamousQuote2/main.ts
@@ -0,0 +1,5 @@
+const famous_person : string = "Hazrat Ali bin Abi Talib (R.A)";
+
+let message : string = '[famous_person] once said, "A moment of patience in the moment of anger prevents thousands of moments of regret."';
+
+console.log(message.replace('[famous_person]', famous_person));
