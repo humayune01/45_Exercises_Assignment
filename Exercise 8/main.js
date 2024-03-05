0f521e8a3bd489a9cca52378cac1a2be00a1b1b1
humayune01 <162161731+humayune01@users.noreply.github.com>
2024-03-05 23:32:03 +0500

    Exercise 8
    You should create four lines that look like this:
    
    console.log(5 + 3)
    
    Your output should simply be four lines with the number 8 appearing once on each line.

---
+++ Exercise 8/main.js
@@ -0,0 +1,8 @@
+console.log(6 + 2);
+console.log('--------------------');
+console.log(12 - 4);
+console.log('--------------------');
+console.log(2 * 4);
+console.log('--------------------');
+console.log(32 / 4);
+console.log('--------------------');
---
+++ Exercise 8/main.ts
@@ -0,0 +1,15 @@
+console.log( 6 + 2 );
+
+console.log('--------------------');
+
+console.log( 12 - 4 );
+
+console.log('--------------------');
+
+console.log( 2 * 4 );
+
+console.log('--------------------');
+
+console.log( 32 / 4 );
+
+console.log('--------------------');
