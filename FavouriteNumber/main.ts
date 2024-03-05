32a3c5fb72c61ef1f9992a17630c4ccc0af52ebb
humayune01 <162161731+humayune01@users.noreply.github.com>
2024-03-05 23:47:15 +0500

    Exercise 10 - Adding Comments
    Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.

--- FavouriteNumber/main.ts
+++ FavouriteNumber/main.ts
@@ -1,3 +1,5 @@
+// Making a variable to store favorite number
 let favoriteNumber : number = 4;
 
+// Printing and revealing my favorite number in the message
 console.log(`My favorite number is ${favoriteNumber}.`);
--- NameCases/main.ts
+++ NameCases/main.ts
@@ -1,15 +1,26 @@
+// create a variable to store any name
 let personName : string = "HaSSan AhMeD";
 
+// making lower case with toLowerCase function
 let lowerName : string = personName.toLowerCase();
 
+// making upper case with toUpperCase function
 let upperName : string = personName.toUpperCase();
 
+// splitting lowername with space to separate the words
 let words = lowerName.split(' ');
 
 let modWords = words.map(word => word[0].toUpperCase() + word.substring(1));
+/**
+ * using map function to modify each word that we separated
+ * then in map function using toUpperCase function to capitalise first
+ * letter and using substring function from index 1 to get the rest of the word.
+ */
 
+// Now joining modified words array with space to make it a single string
 let titleCaseWords = modWords.join(' ');
 
+// Now printing each case (lower/upper/title)
 console.log(lowerName);
 console.log(upperName);
 console.log(titleCaseWords);
