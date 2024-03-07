79adbd0daab93bdedbfb7b1d01e7cd15913f28de
humayune01 <162161731+humayune01@users.noreply.github.com>
2024-03-08 04:00:25 +0500

    Exercise 14 - Guest List
    If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.

---
+++ GuestList/main.js
@@ -0,0 +1,5 @@
+var guestList = ['Azeem', 'Hassan', 'Naved'];
+for (var _i = 0, guestList_1 = guestList; _i < guestList_1.length; _i++) {
+    var guest = guestList_1[_i];
+    console.log("Dear ".concat(guest, ",\nI would like to invite you on dinner tonight.\nIt would be great if you join me.\nThank you"));
+}
---
+++ GuestList/main.ts
@@ -0,0 +1,5 @@
+let guestList : string[] = ['Azeem', 'Hassan', 'Naved'];
+
+for (let guest of guestList) {
+    console.log(`Dear ${guest},\nI would like to invite you on dinner tonight.\nIt would be great if you join me.\nThank you`);
+}
