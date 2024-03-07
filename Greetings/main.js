1517073ebd871b26ac8b81e4d06a7baa41f12064
humayune01 <162161731+humayune01@users.noreply.github.com>
2024-03-08 03:10:56 +0500

    Exercise 12 - Greetings
    Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.

---
+++ Greetings/main.js
@@ -0,0 +1,5 @@
+var friendsList = ['Hassan', 'Zohaib', 'Azeem', 'Naved'];
+for (var _i = 0, friendsList_1 = friendsList; _i < friendsList_1.length; _i++) {
+    var n = friendsList_1[_i];
+    console.log("Hello ".concat(n, ", Its been quite long since we have met. Let's arrange a get together!!"));
+}
---
+++ Greetings/main.ts
@@ -0,0 +1,5 @@
+let friendsList : string[] = ['Hassan', 'Zohaib', 'Azeem', 'Naved'];
+
+for (let n of friendsList) {
+    console.log(`Hello ${n}, Its been quite long since we have met. Let's arrange a get together!!`);
+}
