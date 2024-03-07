a4bfa58aff181ddbc74e831f62e9dad4c42233f6
humayune01 <162161731+humayune01@users.noreply.github.com>
2024-03-08 03:52:05 +0500

    Exercise 13 - Your Own Array
    Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”

---
+++ YourOwnArray/main.js
@@ -0,0 +1,5 @@
+var transport = ['car', 'bike', 'bus'];
+for (var _i = 0, transport_1 = transport; _i < transport_1.length; _i++) {
+    var t = transport_1[_i];
+    console.log("I would like to travel by ".concat(t, "."));
+}
---
+++ YourOwnArray/main.ts
@@ -0,0 +1,5 @@
+let transport : string[] = ['car', 'bike', 'bus'];
+
+for (let t of transport) {
+    console.log(`I would like to travel by ${t}.`);
+}
