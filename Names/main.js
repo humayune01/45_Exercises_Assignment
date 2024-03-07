a12b75755a598d0ff8a7b852f40c1c9cde2e6678
humayune01 <162161731+humayune01@users.noreply.github.com>
2024-03-08 02:08:25 +0500

    Exercise 11 - Names
    Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.

---
+++ Names/main.js
@@ -0,0 +1,16 @@
+var friendsList = ['Hamza', 'Asad', 'Shahroz', 'Farjaad'];
+// + - * / % ++ -- Arithmetic operators
+// ==   >=   <=   <   >  !=  comparison/relational operators
+// = += -= *= /=
+// classic for loop
+// for (let i = 0;  i < friendsList.length; i++) {
+//     console.log(friendsList[i]);
+// }
+// for of loop
+// for (let name of friendsList) {
+//     console.log(name);
+// }
+// for in loop
+for (var index in friendsList) {
+    console.log(friendsList[index]);
+}
---
+++ Names/main.ts
@@ -0,0 +1,6 @@
+let friendsList : string[] = ['Hamza', 'Asad', 'Shahroz', 'Farjaad'];
+
+// for of loop
+for (let name of friendsList) {
+    console.log(name);
+}
