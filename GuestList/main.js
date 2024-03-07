7cdb91e1c15e908adca74e0da8ac8d39e5f27f2d
humayune01 <162161731+humayune01@users.noreply.github.com>
2024-03-08 04:12:56 +0500

    Exercise 15 - Changing Guest List
    You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
    
    • Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
    
    • Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
    
    • Print a second set of invitation messages, one for each person who is still in your list.

--- GuestList/main.js
+++ GuestList/main.js
@@ -3,3 +3,12 @@ for (var _i = 0, guestList_1 = guestList; _i < guestList_1.length; _i++) {
     var guest = guestList_1[_i];
     console.log("Dear ".concat(guest, ",\nI would like to invite you on dinner tonight.\nIt would be great if you join me.\nThank you"));
 }
+var notAttending = 'Hassan';
+var newGuest = 'Zohaib';
+console.log("".concat(notAttending, " is not coming, He got some urgency."));
+var indexMissingGuest = guestList.indexOf(notAttending);
+guestList[indexMissingGuest] = newGuest;
+for (var _a = 0, guestList_2 = guestList; _a < guestList_2.length; _a++) {
+    var guest = guestList_2[_a];
+    console.log("Dear ".concat(guest, ",\nI would like to invite you on dinner tonight.\nIt would be great if you join me.\nThank you"));
+}
--- GuestList/main.ts
+++ GuestList/main.ts
@@ -3,3 +3,16 @@ let guestList : string[] = ['Azeem', 'Hassan', 'Naved'];
 for (let guest of guestList) {
     console.log(`Dear ${guest},\nI would like to invite you on dinner tonight.\nIt would be great if you join me.\nThank you`);
 }
+
+let notAttending : string = 'Hassan';
+let newGuest : string = 'Zohaib';
+
+console.log(`${notAttending} is not coming, He got some urgency.`);
+
+let indexMissingGuest : number = guestList.indexOf(notAttending);
+
+guestList[indexMissingGuest] = newGuest;
+
+for (let guest of guestList) {
+    console.log(`Dear ${guest},\nI would like to invite you on dinner tonight.\nIt would be great if you join me.\nThank you`);
+}
