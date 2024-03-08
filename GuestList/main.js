360303d676c0b7fa996e585766de5de3d731b8e3
humayune01 <162161731+humayune01@users.noreply.github.com>
2024-03-08 05:54:06 +0500

    Exercise 16 - More Guests
    You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
    • Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
    
    • Add one new guest to the beginning of your array.
    
    • Add one new guest to the middle of your array. • Add one new guest to the end of your list. • Print a new set of invitation messages, one for each person in your list.

--- GuestList/main.js
+++ GuestList/main.js
@@ -12,3 +12,12 @@ for (var _a = 0, guestList_2 = guestList; _a < guestList_2.length; _a++) {
     var guest = guestList_2[_a];
     console.log("Dear ".concat(guest, ",\nI would like to invite you on dinner tonight.\nIt would be great if you join me.\nThank you"));
 }
+console.log("Hey Everyone! I've found a bigger dinner table.");
+guestList.unshift('Shahroz');
+var middleIndex = Math.floor(guestList.length / 2);
+guestList.splice(middleIndex, 0, 'Asad');
+guestList.push('Farhan');
+for (var _b = 0, guestList_3 = guestList; _b < guestList_3.length; _b++) {
+    var guest = guestList_3[_b];
+    console.log("Dear ".concat(guest, ",\nI would like to invite you on dinner tonight.\nIt would be great if you join me.\nThank you"));
+}
--- GuestList/main.ts
+++ GuestList/main.ts
@@ -16,3 +16,17 @@ guestList[indexMissingGuest] = newGuest;
 for (let guest of guestList) {
     console.log(`Dear ${guest},\nI would like to invite you on dinner tonight.\nIt would be great if you join me.\nThank you`);
 }
+
+console.log("Hey Everyone! I've found a bigger dinner table.");
+
+guestList.unshift('Shahroz');
+
+let middleIndex : number = Math.floor(guestList.length / 2);
+
+guestList.splice(middleIndex, 0, 'Asad');
+
+guestList.push('Farhan');
+
+for (let guest of guestList) {
+    console.log(`Dear ${guest},\nI would like to invite you on dinner tonight.\nIt would be great if you join me.\nThank you`);
+}
