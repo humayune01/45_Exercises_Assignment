65dbf1d7725fb2bb8d22777fcc2b26397f4c3a5a
humayune01 <162161731+humayune01@users.noreply.github.com>
2024-03-10 01:25:35 +0500

    Exercise 17 - Shrinking Guest List
    You just found out that your new dinner table won’t arrive in time for the dinner, and you have space for only two guests.
    • Start with your program from Exercise 16. Add a new line that prints a message saying that you can invite only two people for dinner.
    
    • Remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list, print a message to that person letting them know you’re sorry you can’t invite them to dinner.
    
    • Print a message to each of the two people still on your list, letting them know they’re still invited.
    
    • Remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program.

--- GuestList/main.js
+++ GuestList/main.js
@@ -13,11 +13,22 @@ for (var _a = 0, guestList_2 = guestList; _a < guestList_2.length; _a++) {
     console.log("Dear ".concat(guest, ",\nI would like to invite you on dinner tonight.\nIt would be great if you join me.\nThank you"));
 }
 console.log("Hey Everyone! I've found a bigger dinner table.");
-guestList.unshift('Shahroz');
-var middleIndex = Math.floor(guestList.length / 2);
+var len = guestList.unshift('Shahroz');
+var middleIndex = Math.round(len / 2);
 guestList.splice(middleIndex, 0, 'Asad');
 guestList.push('Farhan');
 for (var _b = 0, guestList_3 = guestList; _b < guestList_3.length; _b++) {
     var guest = guestList_3[_b];
     console.log("Dear ".concat(guest, ",\nI would like to invite you on dinner tonight.\nIt would be great if you join me.\nThank you"));
 }
+console.log("I can invite only two people for dinner.");
+for (var i = guestList.length - 1; i >= 2; i--) {
+    console.log("Sorry ".concat(guestList[i], "! I can't invite you to dinner."));
+    guestList.pop();
+}
+for (var _c = 0, guestList_4 = guestList; _c < guestList_4.length; _c++) {
+    var guest = guestList_4[_c];
+    console.log("Dear ".concat(guest, ",\nI would like to inform you that you are still invited.\nIt would be great if you join me for dinner.\nThank you"));
+}
+guestList.splice(0, 2);
+console.log(guestList);
--- GuestList/main.ts
+++ GuestList/main.ts
@@ -19,9 +19,9 @@ for (let guest of guestList) {
 
 console.log("Hey Everyone! I've found a bigger dinner table.");
 
-guestList.unshift('Shahroz');
+let len : number = guestList.unshift('Shahroz');
 
-let middleIndex : number = Math.floor(guestList.length / 2);
+let middleIndex = Math.round(len / 2);
 
 guestList.splice(middleIndex, 0, 'Asad');
 
@@ -30,3 +30,18 @@ guestList.push('Farhan');
 for (let guest of guestList) {
     console.log(`Dear ${guest},\nI would like to invite you on dinner tonight.\nIt would be great if you join me.\nThank you`);
 }
+
+console.log("I can invite only two people for dinner.");
+
+for (let i = guestList.length-1; i >= 2; i--) {
+    console.log(`Sorry ${guestList[i]}! I can't invite you to dinner.`);
+    guestList.pop();
+}
+
+for (let guest of guestList) {
+    console.log(`Dear ${guest},\nI would like to inform you that you are still invited.\nIt would be great if you join me for dinner.\nThank you`);
+}
+
+guestList.splice(0, 2);
+
+console.log(guestList);
