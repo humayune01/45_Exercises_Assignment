e10018b45b40a99601dc5b73c9396713dc8b69f4
humayune01 <162161731+humayune01@users.noreply.github.com>
2024-03-16 03:37:32 +0500

    Exercise 2 - Personal Message
    Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”

---
+++ PersonalMessage/main.js
@@ -0,0 +1,2 @@
+var personName = "Ahmed";
+console.log("Salam ".concat(personName, ", would you like to have some tea?"));
---
+++ PersonalMessage/main.ts
@@ -0,0 +1,3 @@
+let personName : string = "Ahmed";
+
+console.log(`Salam ${personName}, would you like to have some tea?`);
