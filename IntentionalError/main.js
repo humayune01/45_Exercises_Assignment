dc318064f916565db50d56235f1e1c57de5bbd41
humayune01 <162161731+humayune01@users.noreply.github.com>
2024-03-24 21:30:50 +0500

    Exercise 22 - Intentional Error
    If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.

---
+++ IntentionalError/main.js
@@ -0,0 +1,4 @@
+var pencilBox = ['pencil', 'pen', 'pointer', 'marker', 'eraser'];
+console.log("One of the Items in my pencil box is, ".concat(pencilBox[5]));
+pencilBox.push('sharpner');
+console.log("One of the Items in my pencil box is, ".concat(pencilBox[5], "!"));
---
+++ IntentionalError/main.ts
@@ -0,0 +1,7 @@
+let pencilBox : string[] = ['pencil', 'pen', 'pointer', 'marker', 'eraser'];
+
+console.log(`One of the Items in my pencil box is, ${pencilBox[5]}`);
+
+pencilBox.push('sharpner');
+
+console.log(`One of the Items in my pencil box is, ${pencilBox[5]}!`);
