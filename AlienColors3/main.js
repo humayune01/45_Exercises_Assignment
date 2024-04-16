b3af977ed5f2d632c772c5b4d73c111a6038e62d
humayune01 <162161731+humayune01@users.noreply.github.com>
2024-04-16 11:18:42 +0500

    Exercise 27 - Alien Colors #3
    Turn your if-else chain from Exercise 5-4 into an if-else chain.
    • If the alien is green, print a message that the player earned 5 points.
    
    • If the alien is yellow, print a message that the player earned 10 points.
    
    • If the alien is red, print a message that the player earned 15 points.
    
    • Write three versions of this program, making sure each message is printed for the appropriate color alien.

---
+++ AlienColors3/main.js
@@ -0,0 +1,30 @@
+var alien_color = "yellow";
+if (alien_color == 'green') {
+    console.log("Player just earned 5 points.");
+}
+else if (alien_color == "red") {
+    console.log("Player just earned 15 points.");
+}
+else {
+    console.log("Player just earned 10 points.");
+}
+alien_color = "green";
+if (alien_color == 'green') {
+    console.log("Player just earned 5 points.");
+}
+else if (alien_color == "red") {
+    console.log("Player just earned 15 points.");
+}
+else {
+    console.log("Player just earned 10 points.");
+}
+alien_color = "red";
+if (alien_color == 'green') {
+    console.log("Player just earned 5 points.");
+}
+else if (alien_color == "red") {
+    console.log("Player just earned 15 points.");
+}
+else {
+    console.log("Player just earned 10 points.");
+}
---
+++ AlienColors3/main.ts
@@ -0,0 +1,46 @@
+
+let alien_color : string = "yellow";
+
+if (alien_color == 'green')
+{
+    console.log("Player just earned 5 points.");
+}
+else if (alien_color == "red")
+{
+    console.log("Player just earned 15 points.");
+}
+else
+{
+    console.log("Player just earned 10 points.");
+}
+
+alien_color = "green";
+
+if (alien_color == 'green')
+{
+    console.log("Player just earned 5 points.");
+}
+else if (alien_color == "red")
+{
+    console.log("Player just earned 15 points.");
+}
+else
+{
+    console.log("Player just earned 10 points.");
+}
+
+alien_color = "red";
+
+if (alien_color == 'green')
+{
+    console.log("Player just earned 5 points.");
+}
+else if (alien_color == "red")
+{
+    console.log("Player just earned 15 points.");
+}
+else
+{
+    console.log("Player just earned 10 points.");
+}
+
